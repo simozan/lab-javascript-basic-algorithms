044675b3dde429b263df71371667fdf593f9e428
“Simone <Simone.zanni.87@gmail.com>
2023-10-11 16:48:11 +0200

    assignement done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Iteration 1: Names and Input
  const hacker1="Marcel"
- const hacker2="Marika"
+ const hacker2="Marina"
  console.log(`The driver's name is ${hacker1}`)
  console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
